1fd33286e1a9cdd71a30cb7081e1262cd937ee8d
saragcorreia <sarag.correia@gmail.com>
2018-12-10 15:30:10 +0000

    autodoc extension

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../src/'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Add any Sphinx extension module names here, as strings
-extensions = ['sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
 
 # Napoleon settings
 napoleon_google_docstring = True
